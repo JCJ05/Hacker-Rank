97739159884f6780d040cf26d575779b1a7f8798
JCJ05 <julitoaguerorengifo@hotmail.com>
2022-08-16 17:52:45 -0500

    Problema Solucionado

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function beautifulDays(i, j, k) {
+    let total = 0;
+    
+    for(let iAr = i; iAr <= j; iAr++){
+    
+        const iTexto = iAr+"";
+        const iReverse = (iAr - parseInt(iTexto.split("").reverse().join(""))) % k;
+        
+        iReverse == 0 && ++total
+    }
+    
+    return total;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const i = parseInt(firstMultipleInput[0], 10);
+
+    const j = parseInt(firstMultipleInput[1], 10);
+
+    const k = parseInt(firstMultipleInput[2], 10);
+
+    const result = beautifulDays(i, j, k);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
